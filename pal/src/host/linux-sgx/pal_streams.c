3ede04ae2ac8248f820d62cae34edf4e76c06d47
Sonali Saha <sonali.saha@intel.com>
2023-06-05 11:09:42 +0530

    mxnet

--- pal/src/host/linux-sgx/pal_streams.c
+++ pal/src/host/linux-sgx/pal_streams.c
@@ -92,7 +92,7 @@ static ssize_t handle_serialize(PAL_HANDLE handle, void** data) {
             break;
         default:
         {
-            log_error("pal_stream.c line 95 %d", ret);
+            log_error("pal_stream.c line 95 %d handle->hdr.type %d", ret, handle->hdr.type);
             return -PAL_ERROR_INVAL;
         }
     }
